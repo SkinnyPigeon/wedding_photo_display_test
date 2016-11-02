a9810448aa1bc5c65f214c6f2706c1f58fc6c4a9
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-11-02 00:41:45 +0000

    timer works well, will need to test the server load

--- client/src/views/MainView.js
+++ client/src/views/MainView.js
@@ -6,34 +6,49 @@ var MainView = function() {
 
 MainView.prototype = {
 
+  randomNumber: function( min, max ) {
+    return Math.floor( Math.random() * ( max - min + 1 )) + min;
+  },
+
   getPictures: function() {
-    var pictureSpace = document.getElementById( 'picture-space' );
-    pictureSpace.innerText = "";
-    var request = new XMLHttpRequest();
-    request.open( 'GET', this.url );
-    request.setRequestHeader("Content-Type", "application/json")
-    request.onload = () => {
-      if( request.status === 200 ) {
-        var pictures = JSON.parse( request.responseText );
-        this.pictures = pictures;
-        this.display();
+    setInterval( function() {
+      var pictureSpace = document.getElementById( 'picture-space' );
+      pictureSpace.innerText = "";
+      var request = new XMLHttpRequest();
+      request.open( 'GET', this.url );
+      request.setRequestHeader("Content-Type", "application/json")
+      request.onload = () => {
+        if( request.status === 200 ) {
+          var pictures = JSON.parse( request.responseText );
+          this.pictures = pictures;
+          this.display();
+        }
       }
-    }
-    request.send( null );
+      request.send( null );
+    }.bind( this ), 7000 );
   },
 
   display: function() {
     var pictureSpace = document.getElementById( 'picture-space' );
-    // pictureSpace;
-    var list = document.createElement( 'ul' );
-    pictureSpace.appendChild( list );
-    for( var i = 0; i < this.pictures.length; i++ ) {
-      var picture = document.createElement( 'img' );
-      var text = this.pictures[i].url;
-      picture.src = text;
-      picture.style.maxWidth = 40 + "%"
-      list.appendChild( picture );
-    }
+    // var list = document.createElement( 'ul' );
+    // pictureSpace.appendChild( list );
+    // for( var i = 0; i < this.pictures.length; i++ ) {
+    //   var picture = document.createElement( 'img' );
+    //   var text = this.pictures[i].url;
+    //   picture.src = text;
+    //   picture.style.maxWidth = 40 + "%"
+    //   list.appendChild( picture );
+    // }
+    pictureSpace.innerText = "";
+    var max = this.pictures.length - 1;
+    var i = this.randomNumber( 0, max );
+    console.log( i );
+    console.log( max );
+    var picture = document.createElement( 'img' );
+    var url = this.pictures[i].url;
+    picture.src = url;
+    picture.style.maxWidth = 50 + "%";
+    pictureSpace.appendChild( picture );
   },
 
 
