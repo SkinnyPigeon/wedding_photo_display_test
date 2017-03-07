106ff94ee4bcdb54b7fb6b3eaa02fc174c1611ae
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-03-07 22:36:02 +0000

    fairly breaking it so just about to rebuild in a smarter order

--- client/src/views/MainView.js
+++ client/src/views/MainView.js
@@ -1,17 +1,60 @@
 var MainView = function() {
   this.pictures = [];
   this.url = "https://wedding--photo-test.herokuapp.com/pictures";
+  this.photos = [];
+  this.position = 0;
+  this.numberOfPhotos = 0;
+  this.lastPhoto = 1;
+  this.loopCheck = false;
+  this.initialLength = 0;
+  this.newLength = 0;
   this.getPictures();
 }
 
 MainView.prototype = {
 
-  randomNumber: function( min, max ) {
-    return Math.floor( Math.random() * ( max - min + 1 )) + min;
+  noOfPhotos: function() {
+    return this.photos.length;
   },
 
+  addPhoto: function( photo ) {
+    this.photos.push( photo );
+    this.numberOfPhotos += 1;
+  },
+
+  pickPhoto: function() {
+    this.lengthCheck();
+    this.newPhotoCheck();
+    this.randomNumber();
+    var link = this.photos[ this.position ];
+    this.position += 1;
+    return link;
+  },
+
+  lengthCheck: function() {
+    if( this.position >= this.numberOfPhotos ) {
+      this.lastPhoto = this.position;
+      this.position = 0;
+      this.loopCheck = true;
+    };
+  },
+
+  newPhotoCheck: function() {
+    if( this.numberOfPhotos > this.lastPhoto && this.loopCheck ) {
+      this.position = this.lastPhoto;
+      this.loopCheck = false;
+    }
+  },
+
+  randomNumber: function() {
+    if( this.loopCheck ) {
+      this.position = Math.floor(Math.random() * (this.lastPhoto - 1));
+    }
+  },
+
+
   getPictures: function() {
-    setInterval( function() {
+    // setInterval( function() {
       var pictureSpace = document.getElementById( 'picture-space' );
       pictureSpace.innerText = "";
       var request = new XMLHttpRequest();
@@ -25,27 +68,19 @@ MainView.prototype = {
         }
       }
       request.send( null );
-    }.bind( this ), 7000 );
+    // }.bind( this ), 7000 );
   },
 
   display: function() {
+          console.log( this.pictures );
     var pictureSpace = document.getElementById( 'picture-space' );
-    // var list = document.createElement( 'ul' );
-    // pictureSpace.appendChild( list );
-    // for( var i = 0; i < this.pictures.length; i++ ) {
-    //   var picture = document.createElement( 'img' );
-    //   var text = this.pictures[i].url;
-    //   picture.src = text;
-    //   picture.style.maxWidth = 40 + "%"
-    //   list.appendChild( picture );
-    // }
     pictureSpace.innerText = "";
     var max = this.pictures.length - 1;
-    var i = this.randomNumber( 0, max );
-    console.log( i );
-    console.log( max );
+    var i = this.randomNumber();
+    console.log( this.pictures[i] );
     var picture = document.createElement( 'img' );
     var url = this.pictures[i].url;
+    console.log( url )
     picture.src = url;
     picture.style.maxWidth = 50 + "%";
     pictureSpace.appendChild( picture );
